e82dd4e664406a06f3ff8b22394ac132bbef7b89
Nevimmu <nevimmu@gmail.com>
2025-06-30 05:06:21 +0200

    feat(sidebar): control sidebar with a list

---
+++ frontend/src/components/NavlinkComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+defineProps({
+	name: {
+		type: String,
+		required: true,
+	},
+	to: {
+		type: String,
+		required: true,
+	},
+	icon: {
+		type: String,
+		required: false,
+		default: null,
+	},
+})
+</script>
+
+<template>
+	<RouterLink :to="to" active-class="is-active">
+		<template v-if="icon">
+			<span class="icon-text">
+				<span class="icon">
+					<i :class="icon"></i>
+				</span>
+				<span>{{ name }}</span>
+			</span>
+		</template>
+		<template v-else>
+			{{ name }}
+		</template>
+	</RouterLink>
+</template>
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -3,6 +3,12 @@ import { RouterLink } from 'vue-router'
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useSidebarStore } from '@/stores/sidebar'
 import { onClickOutside } from '@vueuse/core'
+import NavlinkComponent from '@/components/NavlinkComponent.vue'
+
+const navLinks = [
+	{ name: 'Home', to: '/', icon: 'fas fa-home' },
+	{ name: 'About', to: '/about' },
+]
 
 const sidebarStore = useSidebarStore()
 const sidebarRef = ref(null)
@@ -21,17 +27,12 @@ onClickOutside(sidebarRef, () => {
 		<aside class="menu">
 			<p class="menu-label">Menu</p>
 			<ul class="menu-list">
-				<li>
-					<RouterLink to="/" active-class="is-active">
-						<span class="icon-text">
-							<span class="icon">
-								<i class="fas fa-home"></i>
-							</span>
-							<span>Home</span>
-						</span>
-					</RouterLink>
-				</li>
-				<li><RouterLink to="/about" active-class="is-active">About</RouterLink></li>
+				<NavlinkComponent
+					v-for="link in navLinks"
+					:name="link.name"
+					:to="link.to"
+					:icon="link.icon"
+				/>
 			</ul>
 		</aside>
 	</div>
