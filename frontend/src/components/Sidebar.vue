54cdde86f237c527a1b77eda09876dbb651538ee
Nevimmu <nevimmu@gmail.com>
2025-07-03 01:46:53 +0200

    feat(Frontend): removed Bulma and related custom CSS

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -37,26 +37,3 @@ onClickOutside(sidebarRef, () => {
 		</aside>
 	</div>
 </template>
-
-<style lang="scss">
-@use 'bulma/sass/utilities/initial-variables' as iv;
-@use 'bulma/sass/utilities/derived-variables' as dv;
-
-@media screen and (max-width: iv.$tablet) {
-	.sidebar {
-		position: fixed;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		width: 250px;
-		z-index: 100;
-		transition: transform 0.3s ease;
-		transform: translateX(-100%);
-		// background-color: dv.$background;
-	}
-
-	.sidebar.is-open {
-		transform: translateX(0);
-	}
-}
-</style>
